9625e7bc245f4e0f925c49265b2cecf3bdbe0913
Waddah <waddah.ja@gmail.com>
2025-09-14 23:07:24 +0300

    setup e2e testing and add test cases for parseVerb

--- src/utils/wiktionary.ts
+++ src/utils/wiktionary.ts
@@ -1,6 +1,5 @@
 async function getWebpageHtmlContent(page: string): Promise<string> {
   const url = `https://ru.wiktionary.org/wiki/${encodeURIComponent(page)}`;
-  console.log(url);
   const response = await fetch(url);
   return await response.text();
 }
